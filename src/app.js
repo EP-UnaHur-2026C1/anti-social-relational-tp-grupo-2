913ab9f8fc5cf957a734b1ac61cb1cafdfdbcde0
jorditan <matiasowjordan@gmail.com>
2026-05-28 08:41:10 -0300

    feat: :memo: exponiendo rutas de swagger para poder visualizarlas

--- src/app.js
+++ src/app.js
@@ -8,11 +8,19 @@ const postRoutes = require('./routes/post.routes');
 const commentRoutes = require('./routes/comment.routes');
 const tagRoutes = require('./routes/tag.routes');
 
+// -- Esto es para la documentación de Swagger, hay que instalar swagger-ui-express y yamljs para poder usarlo --
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 app.use(express.json());
 app.use('/posts', postRoutes);
 app.use('/comments', commentRoutes);
 app.use('/tags', tagRoutes);
 app.use('/users', userRoutes);
+
+//  -- Esto es para poder acceder a la documentación desde una ruta -- 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, async () => {
   await db.sequelize.sync();
